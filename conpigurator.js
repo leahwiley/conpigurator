8dc872a196b7639dcb64ac5c406041db9770215d
Nathaniel Wiley <nwiley86@gmail.com>
2016-10-18 00:13:12 -0400

    Initial Code Commit
    conpiguator.js uses vanilla javascript. So, it has no dependencies, but it is not IE compatible.
    
    conpigurator.jquery.js is dependent on jQuery methods to become more succinct and provide IE compatibility. If you are already loading jQuery, this file has an even smaller footprint. If IE compatibility is necessary, jQuery + this file will have you conpigurating in a flash.

---
+++ conpigurator.js
@@ -0,0 +1,32 @@
+;var conpigurator = {
+	active:false,
+	handleChange:function(event){
+		if(event.target.className.includes('conpiguratorInput')){
+			if(event.target.value && event.target.value !== '' && typeof event.target.value === 'string'){
+				if(event.target.dataset){
+					var pigment = event.target.value;
+					if(pigment.substring(0,1) !== '#') pigment = '#'+pigment;
+					pigment = (pigment.length > 7)? pigment.substring(0,7) : pigment;
+					conpigurator.conpigurateElements(event.target.dataset.conpigbg,'backgroundColor',pigment);
+					conpigurator.conpigurateElements(event.target.dataset.conpigborder,'borderColor',pigment);
+					conpigurator.conpigurateElements(event.target.dataset.conpigtext,'color',pigment);
+				}
+			}
+		}
+	},
+	conpigurateElements:function(selector,colorProperty,pigment){
+		if(selector){
+			colorProperty = colorProperty || 'backgroundColor';
+			var arrElements = document.querySelectorAll(selector);
+			for(var i=0;i<arrElements.length;i++){
+				arrElements[i].style[colorProperty] = pigment;
+			}
+		}
+	},
+	init:function(){
+		if(!conpigurator.active){
+			document.body.onchange(function(event){conpigurator.handleChange(event);});
+			conpigurator.active = true;
+		}
+	}
+};
