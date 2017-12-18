f8cfd826c4a7b2377370c3acc91991d99a575d64
Nathaniel Wiley <nwiley86@gmail.com>
2017-12-18 10:26:21 -0500

    Protoype Build Done
    Standalone vanilla JS class constructor driven by element IDs and Hex values

--- conpigurator.js
+++ conpigurator.js
@@ -1,32 +1,38 @@
-;var conpigurator = {
-	active:false,
-	handleChange:function(event){
-		if(event.target.className.includes('conpiguratorInput')){
-			if(event.target.value && event.target.value !== '' && typeof event.target.value === 'string'){
-				if(event.target.dataset){
-					var pigment = event.target.value;
-					if(pigment.substring(0,1) !== '#') pigment = '#'+pigment;
-					pigment = (pigment.length > 7)? pigment.substring(0,7) : pigment;
-					conpigurator.conpigurateElements(event.target.dataset.conpigbg,'backgroundColor',pigment);
-					conpigurator.conpigurateElements(event.target.dataset.conpigborder,'borderColor',pigment);
-					conpigurator.conpigurateElements(event.target.dataset.conpigtext,'color',pigment);
-				}
-			}
+;var ConpiguratorArray = [];
+function Conpigurator (id) {
+	this.el = document.getElementById(id);
+	this.el.setAttribute('data-conpigurator-index',ConpiguratorArray.length);
+	this.targets = [];
+	this.el.addEventListener('change',function(event){
+		ConpiguratorArray[Number(event.target.getAttribute('data-conpigurator-index'))].update();
+	});
+	this.update = function () {
+		var hex = this.el.value;// Assumes this.el.value is a Hexadecimal value
+		if(hex.substring(0,1) !== '#') hex = '#'+hex;
+		for(var idx in this.targets){
+			document.getElementById(this.targets[idx].ID).style[this.targets[idx].prop] = hex;
 		}
-	},
-	conpigurateElements:function(selector,colorProperty,pigment){
-		if(selector){
-			colorProperty = colorProperty || 'backgroundColor';
-			var arrElements = document.querySelectorAll(selector);
-			for(var i=0;i<arrElements.length;i++){
-				arrElements[i].style[colorProperty] = pigment;
-			}
-		}
-	},
-	init:function(){
-		if(!conpigurator.active){
-			document.body.addEventListener('change',function(event){conpigurator.handleChange(event);});
-			conpigurator.active = true;
-		}
-	}
-};
+		return this;
+	};
+	this.addTarget = function (id,property) {// No safeguards against ID or property name typos OR incompatible property names
+		id = id || '';
+		property = property || 'backgroundColor';
+		this.targets.push({ID:id,prop:property});
+		return this;
+	};
+	ConpiguratorArray.push(this);
+	return this;
+}
+/*	
+	CONPIGURATOR By Nathaniel Wiley
+	==========
+	var myConpigurator = new Conpigurator('inputID')
+		.addTarget('targetID','backgroundColor')
+		.addTarget('targetID','color')...;
+	myConpigurator.addTarget('targetID','CSS color property');// Add 
+	myConpigurator.update();// Force application of myConpigurator
+	==========
+	1.0.0 - 2017-12-18 
+		- Functionality with Hexadecimal input proven in:
+		IE9, IE10, Edge, Firefox 56.0.2, and Chrome 63.0.3239.84
+*/
